9217b5d6a4c2b6d28f9e933f83a9af7072523a21
Hector Alfaro <halfaro1@valenciacollege.edu>
2016-01-06 22:59:50 -0500

    initial

---
+++ week2-hello-world.py
@@ -0,0 +1,9 @@
+# replace the contents of this comment with your full name
+
+# write a program that:
+# 1. greets the user in English
+# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
+# 3. displays the greeting in the chosen language
+# 4. exits
+
+# make sure that your code contains comments explaining your logic!
