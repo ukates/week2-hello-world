5c92951565b430ccc9b63aa73a301fdfc7a79842
Derrick Cates <dcates@mail.valenciacollege.edu>
2016-01-17 22:13:43 -0500

    Attempt 5 week 2 assignment helloworld

--- helloworld.py
+++ helloworld.py
@@ -8,20 +8,19 @@
 
 # make sure that your code contains comments explaining your logic!
 
+def helloworld():
+    print('Hello World!') # this is what will display as printed text
+    print() #this will display a blank line 
+    print('Type one of the numbers below and I will greet you in that language!')
+    print('    1. Spanish')
+    print('    2. Chinese')
+    print('    3. Italian')
 
-print('Hello World!') # this is what will display as printed text
-print() #this will display a blank line 
-print('Type one of the numbers below and I will greet you in that language!')
-print('    1. Spanish')
-print('    2. Chinese')
-print('    3. Italian')
+    choice = input() # This is an assignment statement
 
-choice = input() # This is an assignment statement
-
-if choice == str(1): # If the user types the number 1 this statement would be true
-    print('Hola!')
-if choice == str(2): 
-    print('Ni Hao')
-if choice == str(3):
-    print('Ciao!')
-exit 
+    if choice == str(1): # If the user types the number 1 this statement would be true
+        print('Hola!')
+    if choice == str(2): 
+        print('Ni Hao')
+    if choice == str(3):
+        print('Ciao!')
