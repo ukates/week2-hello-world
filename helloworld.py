3b43b41dfdc5d24ef7b48abcd94855c1f7cfd98e
Derrick Cates <dcates@mail.valenciacollege.edu>
2016-01-17 21:21:59 -0500

    Week 2 Assignment helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-# replace the contents of this comment with your full name
+# Derrick Cates
 
 # write a program that:
 # 1. greets the user in English
@@ -8,3 +8,25 @@
 
 # make sure that your code contains comments explaining your logic!
 
+
+print('Hello World!') # this is what will display as printed text when the program begins. 
+print() #this will display a blank line 
+print('Type one of the numbers below and I will greet you in that language!')
+print('    1. Spanish')
+print('    2. Chinese')
+print('    3. Huttese') # Huttese is the language of the Huts in StarWars!
+
+choice = input() # This is an assignment statement, the user makes a choice by typing a number. That input is assigned to the word 'choice' which is used to determine if the "if" statement is true or false. 
+
+if choice == str(1): # If the user types the number 1 this statement would be true because choice would equal the input and the input would = 1.  
+    print('Hola!')
+if choice == str(2): 
+    print('Ni Hao')
+if choice == str(3): #previous if statements are made false by users input, and the input = 3 then the huttese greeting will display!
+    print('Achuta!')
+
+
+
+
+    
+    
